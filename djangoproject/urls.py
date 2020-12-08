b57b1b7a4c504987cd9c35e8333d710a9e99675b
Archana <archanaelisala@gmail.com>
2020-12-08 11:48:11 +0530

    djangotot

---
+++ djangoproject/urls.py
@@ -0,0 +1,33 @@
+"""djangoproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from djtot import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.hello),
+    path('about/',views.about),
+    path('string_value/<str:name>/',views.String),
+    path('table/<int:num>',views.table),
+    path('twoparameters/<str:name1>/<int:age>',views.twoVar),
+    path('cssex/',views.cssexample),
+    path('login',views.Login),
+    path('register',views.Register),
+    path('javascript',views.Javascript_Ex)
+
+
+]
---
+++ djtot/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def hello(request):
+	return HttpResponse("<h1>Welcome django Page</h1>")
+
+def about(request):
+	return HttpResponse("<h2 style='background-color:pink'> Hellooo.....</h2>")
+
+def String(y,name):
+	return HttpResponse("<h2>Hii Welcome to online class {}</h2>".format(name))
+
+#Day1 Task
+def table(h,num):
+	k = ""
+	for i in range(1,11):
+		k += " {} X {:02} = {:02}".format(num,i,num*i)+"<br/>"
+	#print(k)
+	return HttpResponse(k)
+
+#Task2
+def twoVar(request,name1,age):
+	#return HttpResponse("<h1> Welcome to this Page</h1>"+name1+"<h1>Age is</h1> %d"+age)
+	print(name1,age)
+	return render(request,'fy/index.html',{'n':name1,'a':age})
+
+
+def cssexample(request):
+	return render(request,'fy/demo.html')
+
+
+
+def Login(request):
+	return render(request,'fy/login.html')
+
+def Register(request):
+	return render(request,'fy/register.html')
+
+
+
+def Javascript_Ex(request):
+	return render(request,'fy/math.html')
