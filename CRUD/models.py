d609a9f67d6a831c9910b893ef5ee1c7698103b3
Archana <archanaelisala@gmail.com>
2020-12-14 13:56:05 +0530

    friday 11-12-2020

--- CRUD/models.py
+++ CRUD/models.py
@@ -7,3 +7,25 @@ class Student(models.Model):
 	age = models.IntegerField()
 	phone = models.CharField(max_length=10)
 	email = models.EmailField(max_length=30)
+	# address = models.TextField(null=True)
+
+
+	def __str__(self):
+		return self.name+" "+self.email
+
+
+
+class Emp(models.Model):
+	Ename = models.CharField(max_length=30)
+	Eid = models.CharField(max_length=20)
+	Esal = models.IntegerField()
+	mobile = models.CharField(max_length=10)
+	Email = models.CharField(max_length=30)
+	Eaddress = models.TextField(max_length=30)
+	#Eresume = models.FileField(upload_to = 'path/')
+	#Eimg = models.ImageField(upload_to=None,height_field=None,width_field=None,max_length=100)
+
+
+	def __str__(self):
+		return self.Ename
+
